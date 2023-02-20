06915c7d9bb4785c2231b1981bad033e0b9ab307
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-02-20 15:45:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+include_once __DIR__ . '/models/product-info.php';
+
+$dog_food = new Product('Cibo', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.carrefour.it%2Fp%2Fcarrefour-adult-bocconi-in-salsa-con-pollo-tacchino-e-ortaggi-per-cani-405-g%2F3560070931118.html&psig=AOvVaw1rEyruDmZmS4kAHQakJyyg&ust=1676990323582000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPjuk_appP0CFQAAAAAdAAAAABAE', 'Cibo in scatola per cani', 5);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP 2</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+
+</body>
+</html>
